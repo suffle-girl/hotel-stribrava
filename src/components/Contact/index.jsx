4b2781c2190c0686574f906f3199ba6a2ab35c43
Denisa Zemanova <xdenisa.zemanova@gmail.com>
2024-05-25 12:19:38 +0200

    Úprava barev a přidání komponenty Kontakt

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,29 @@
 import "./style.css"
+import mapa from "./img/mapa.png";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <section class="dark">
+    <div class="container columns-2">
+      <div class="columns">
+        <h2>Kontakt</h2>
+        <p>
+          Hotel Stříbrava
+        </p>
+        <p>
+          Na Kamenné lávce 12
+        </p>
+        <p>
+          317 24 Libnice nad Stříbravou
+        </p>
+        <p>
+          recepce@hotelstribrava.cz
+        </p>
+      </div>
+      <img src={mapa} />
+    </div>
+  </section>
+  ) 
 }
 
 
