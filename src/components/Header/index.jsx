8854ca7b19279048d7c77dc59bd45c499ba0d76b
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-25 10:52:12 +0200

    Úprava comp Header a základních stylů.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import "./style.css"
+import "./style.css";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
-
-
+    <div class="banner">
+      <div class="banner__stripe">
+        <div class="container">
+          <h1>Hotel Stříbrava</h1>
+          <p class="lead">
+            Rodinný hotel v malebné krajině Českého středohoří.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
